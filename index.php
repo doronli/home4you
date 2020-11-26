814fba81bc89fa2f5920670381e91fa7fc9c1475
Doron <lisbona107@gmail.com>
2020-11-26 12:05:43 +0200

    lp for moshe project

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>קניית דירה ב 4 צעדים פשוטים</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <?php include "./components/tracking.php"
+    ?>
+</head>
+
+<body class="lp-body" dir="rtl">
+    <div class="container">
+        <div class="row justify-content-center pt-5">
+            <div class="d-flex align-items-center">
+                <img src="./assets/img/favicon.png" alt="דירה" height="70px">
+                <h3 class="pr-3">
+                    ליווי אישי ומקצועי עד קבלת מפתחות הדירה
+                </h3>
+            </div>
+            <h2 class="mt-5">
+                מזל טוב :) אם הגעת לדף זה כנראה שאתה בדרך הנכונה לקניית הדירה הבאה שלך
+            </h2>
+            <p class="services">
+                המומחים שלנו ידאגו לך ל:
+                <br>
+
+                <span>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                    </svg>
+                    בחירת דירה 
+                </span>
+                <br>
+                <span>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                    </svg>
+                    ייעוץ עסקי 
+                </span>
+                <br>
+                <span>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                    </svg>
+                    ייעוץ משכנתאות 
+                </span>
+                <br>
+                <span>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z" />
+                    </svg>
+                    בדיקת חוזה מול העורך דין
+                </span>
+            </p>
+
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <h4 class="mt-5 text-center">
+                    לפגישת ייעוץ חינם השאר לנו את פרטיך ונחזור אליך בהקדם
+                </h4>
+                <form id="form-lp" class="mt-4" action="" onsubmit="">
+                    <div class="form-group col-12 col-md-6 mx-auto">
+                        <input type="text" class="form-control" id="name" name="name" placeholder="שם">
+
+                        <input type="text" class="form-control mt-4" id="phone" name="phone" placeholder="טלפון">
+                        <button class="btn btn-primary mt-4 col-12" type="submit">שלח</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+        
+    </div>
+
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</body>
+
+</html>
